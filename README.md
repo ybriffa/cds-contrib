10389be077d61dfbc6280b1315368c33dbfdd341
François Samin <francois.samin@corp.ovh.com>
2016-10-27 09:33:02 +0200

    add sources

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# cds-contrib
+# CDS Contrib
+
+Here you'll find extensions ready to use with [CDS](https://github.com/ovh/cds).
+
+CDS support several kind of extensions:
+
+- Plugins
+- Templates
+- Secret Backends
+
+See [CDS documentation](https://github.com/ovh/cds) for more details.
+
+## Plugins
+
+- [Kafka Publisher](https://github.com/ovh/cds-contrib/tree/master/plugin-kafka-publish)
+- [Mesos/Marathon Deployment](https://github.com/ovh/cds-contrib/tree/master/plugin-marathon)
+
+## Templates
+
+*No template available yet*
+
+## Secret Backends
+
+- [Vault Secret Backend](https://github.com/ovh/cds-contrib/tree/master/secret-backend-vault)
+
+## Contributions
+
+By convention, plugins must have prefix `plugin`, templates  must have prefix `templates`, secret backends must have prefix `secret-backend`.
+
+If adding a package directly, don't forget to create a README.md inside with full description of your extension and a AUTHORS file.
+
+Read [CONTRIBUTING guide](CONTRIBUTING.md) for more details about contributions.
+
+## License
+
+This work is under the BSD license, see the [LICENSE](LICENSE) file for details.
---
+++ plugin-marathon/marathon.go
@@ -0,0 +1,368 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"math"
+	"net/http"
+	"net/url"
+	"os"
+	"path"
+	"path/filepath"
+	"regexp"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/facebookgo/httpcontrol"
+	"github.com/gambol99/go-marathon"
+	"github.com/ovh/cds/sdk/plugin"
+	"github.com/xeipuuv/gojsonschema"
+)
+
+//MarathonPlugin is our marathon plugin to manage app deployment
+type MarathonPlugin struct {
+	plugin.Common
+}
+
+//Name return plugin names. It must me the same as the binary file
+func (m MarathonPlugin) Name() string {
+	return "plugin-marathon"
+}
+
+//Description explains the purpose of the plugin
+func (m MarathonPlugin) Description() string {
+	return `This action helps you to deploy on Mesos/Marathon. Provide a marathon.json file to configure deployment.
+Your marathon.json file can be templated with cds variables "{{.cds.variables}}". Enable "waitForDeployment" option to ensure deployment is successfull.`
+}
+
+//Author of the plugin
+func (m MarathonPlugin) Author() string {
+	return "François SAMIN <francois.samin@corp.ovh.com>"
+}
+
+//Parameters return parameters description
+func (m MarathonPlugin) Parameters() plugin.Parameters {
+	params := plugin.NewParameters()
+
+	params.Add("url", plugin.StringParameter,
+		"Marathon URL http://127.0.0.1:8081,http://127.0.0.1:8082,http://127.0.0.1:8083",
+		"{{.cds.env.marathonHost}}")
+	params.Add("user", plugin.StringParameter,
+		"Marathon User (please use project, application or environment variables)",
+		"{{.cds.env.marathonUser}}")
+
+	params.Add("password", plugin.StringParameter,
+		"Marathon Password (please use project, application or environment variables)",
+		"{{.cds.env.marathonPassword}}")
+
+	params.Add("configuration", plugin.StringParameter,
+		"Marathon application configuration file (json format)",
+		"marathon.json")
+
+	params.Add("waitForDeployment", plugin.BooleanParameter,
+		`Wait for instances deployment.
+If set, CDS will wait for all instances to be deployed until timeout is over. All instances deployment must be done to get a successfull result.
+If not set, CDS will consider a successfull result if marathon accepts the provided configuration.`,
+		"true")
+
+	params.Add("timeout", plugin.NumberParameter,
+		`Marathon deployment timeout (seconds). Used only if "waitForDeployment" is true. `,
+		"120")
+
+	return params
+}
+
+//Run execute the action
+func (m MarathonPlugin) Run(a plugin.IAction) plugin.Result {
+	//Get arguments
+	URL := a.Arguments().Get("url")
+	user := a.Arguments().Get("user")
+	password := a.Arguments().Get("password")
+	tmplConf := a.Arguments().Get("configuration")
+	waitForDeploymentStr := a.Arguments().Get("waitForDeployment")
+	timeoutStr := a.Arguments().Get("timeout")
+
+	//Parse arguments
+	waitForDeployment, err := strconv.ParseBool(waitForDeploymentStr)
+	if err != nil {
+		plugin.SendLog(a, "PLUGIN-MARATHON", "Error parsing waitForDeployment value : %s\n", err.Error())
+		return plugin.Fail
+	}
+
+	timeout, err := strconv.Atoi(timeoutStr)
+	if err != nil {
+		plugin.SendLog(a, "PLUGIN-MARATHON", "Error parsing timeout value :  %s\n", err.Error())
+		return plugin.Fail
+	}
+
+	//Custom http client with 3 retries
+	httpClient := &http.Client{
+		Transport: &httpcontrol.Transport{
+			RequestTimeout: time.Minute,
+			MaxTries:       3,
+		},
+	}
+
+	plugin.SendLog(a, "PLUGIN-MARATHON", "Connecting on %s\n", URL)
+	config := marathon.NewDefaultConfig()
+	config.URL = URL
+	config.HTTPBasicAuthUser = user
+	config.HTTPBasicPassword = password
+	config.HTTPClient = httpClient
+
+	//Connecting to marathon
+	client, err := marathon.NewClient(config)
+	if err != nil {
+		plugin.SendLog(a, "PLUGIN-MARATHON", "Connection failed on %s\n", URL)
+		return plugin.Fail
+	}
+
+	//Run tmpl on configuration file to replace all cds variables
+	conf, err := tmplApplicationConfigFile(a, tmplConf)
+	if err != nil {
+		return plugin.Fail
+	}
+	defer os.RemoveAll(conf)
+
+	//Validate json file and load application
+	appConfig, err := parseApplicationConfigFile(a, conf)
+	if err != nil {
+		return plugin.Fail
+	}
+
+	//Allways put cds.version labels
+	if appConfig.Labels == nil {
+		appConfig.Labels = &map[string]string{}
+	}
+	(*appConfig.Labels)["CDS_VERSION"] = a.Arguments().Get("cds.version")
+
+	plugin.SendLog(a, "PLUGIN-MARATHON", "Configuration File %s: OK\n", tmplConf)
+
+	//Searching for application
+	val := url.Values{"ID": []string{appConfig.ID}}
+	applications, err := client.Applications(val)
+	if err != nil {
+		plugin.SendLog(a, "PLUGIN-MARATHON", "Failed to list applications : %s\n", err.Error())
+		return plugin.Fail
+	}
+
+	var appExists bool
+	if len(applications.Apps) != 0 {
+		appExists = true
+	}
+
+	//Update or create application
+	if appExists {
+		if _, err := client.UpdateApplication(appConfig, true); err != nil {
+			plugin.SendLog(a, "PLUGIN-MARATHON", "Application %S update failed :%s\n", appConfig.ID, err)
+		}
+		plugin.SendLog(a, "PLUGIN-MARATHON", "Application updated %s: OK\n", appConfig.ID)
+	} else {
+		if _, err := client.CreateApplication(appConfig); err != nil {
+			plugin.SendLog(a, "PLUGIN-MARATHON", "Application %S creation failed :%s\n", appConfig.ID, err)
+		}
+		plugin.SendLog(a, "PLUGIN-MARATHON", "Application creation %s: OK\n", appConfig.ID)
+	}
+
+	//Check deployments
+	if waitForDeployment {
+		ticker := time.NewTicker(time.Second * 5)
+		go func() {
+			t0 := time.Now()
+			for t := range ticker.C {
+				delta := math.Floor(t.Sub(t0).Seconds())
+				plugin.SendLog(a, "PLUGIN-MARATHON", "Application %s deployment in progress [%d seconds] please wait...\n", appConfig.ID, int(delta))
+			}
+		}()
+
+		plugin.SendLog(a, "PLUGIN-MARATHON", "Application %s deployment in progress please wait...\n", appConfig.ID)
+		deployments, err := client.ApplicationDeployments(appConfig.ID)
+		if err != nil {
+			plugin.SendLog(a, "PLUGIN-MARATHON", "Failed to list deployments : %s\n", err.Error())
+			ticker.Stop()
+			return plugin.Fail
+		}
+
+		if len(deployments) == 0 {
+			ticker.Stop()
+			return plugin.Success
+		}
+
+		wg := &sync.WaitGroup{}
+		var successChan = make(chan bool, len(deployments))
+		for _, deploy := range deployments {
+			wg.Add(1)
+			go func(id string) {
+				go func() {
+					time.Sleep((time.Duration(timeout) + 1) * time.Second)
+					ticker.Stop()
+					successChan <- false
+					wg.Done()
+				}()
+
+				if err := client.WaitOnDeployment(id, time.Duration(timeout)*time.Second); err != nil {
+					plugin.SendLog(a, "PLUGIN-MARATHON", "Error on deployment %s: %s\n", id, err.Error())
+					ticker.Stop()
+					successChan <- false
+					wg.Done()
+					return
+				}
+
+				plugin.SendLog(a, "PLUGIN-MARATHON", "Deployment %s succeeded", id)
+				ticker.Stop()
+				successChan <- true
+				wg.Done()
+			}(deploy.DeploymentID)
+		}
+
+		wg.Wait()
+
+		var success = true
+		for b := range successChan {
+			success = success && b
+			if len(successChan) == 0 {
+				break
+			}
+		}
+		ticker.Stop()
+		close(successChan)
+
+		if success {
+			return plugin.Success
+		}
+		return plugin.Fail
+	}
+
+	return plugin.Success
+}
+
+func tmplApplicationConfigFile(a plugin.IAction, filepath string) (string, error) {
+	//Read marathon.json
+	buff, err := ioutil.ReadFile(filepath)
+	if err != nil {
+		plugin.SendLog(a, "PLUGIN-MARATHON", "Configuration file error : %s\n", err)
+		return "", err
+	}
+
+	fileContent := string(buff)
+	data := map[string]string{}
+	for k, v := range a.Arguments().Data {
+		kb := strings.Replace(k, ".", "__", -1)
+		data[kb] = v
+		re := regexp.MustCompile("{{." + k + "(.*)}}")
+		sm := re.FindStringSubmatch(fileContent)
+		for {
+			if len(sm) > 0 {
+				fileContent = strings.Replace(fileContent, sm[0], "{{."+kb+sm[1]+"}}", -1)
+			} else {
+				break
+			}
+		}
+	}
+
+	funcMap := template.FuncMap{
+		"title":  strings.Title,
+		"lower":  strings.ToLower,
+		"upper":  strings.ToUpper,
+		"escape": Escape,
+	}
+
+	t, err := template.New("file").Funcs(funcMap).Parse(fileContent)
+	if err != nil {
+		plugin.SendLog(a, "PLUGIN-MARATHON", "Invalid template format: %s\n", err.Error())
+		return "", err
+	}
+
+	out, err := ioutil.TempFile(os.TempDir(), "marathon.json")
+	if err != nil {
+		plugin.SendLog(a, "PLUGIN-MARATHON", "Error writing temporary file : %s\n", err.Error())
+		return "", err
+	}
+	outPath := out.Name()
+	if err := t.Execute(out, data); err != nil {
+		plugin.SendLog(a, "PLUGIN-MARATHON", "Failed to execute template: %s\n", err.Error())
+		return "", err
+	}
+
+	return outPath, nil
+}
+
+func parseApplicationConfigFile(a plugin.IAction, f string) (*marathon.Application, error) {
+	//Read marathon.json
+	buff, err := ioutil.ReadFile(f)
+	if err != nil {
+		plugin.SendLog(a, "PLUGIN-MARATHON", "Configuration file error : %s\n", err)
+		return nil, err
+	}
+
+	//Parse marathon.json
+	appConfig := &marathon.Application{}
+	if err := json.Unmarshal(buff, appConfig); err != nil {
+		plugin.SendLog(a, "PLUGIN-MARATHON", "Configuration file parse error : %s\n", err)
+		return nil, err
+	}
+
+	//Validate with official schema : https://mesosphere.github.io/marathon/docs/generated/api.html#v2_apps_post
+	wd, err := os.Getwd()
+	if err != nil {
+		plugin.SendLog(a, "PLUGIN-MARATHON", "Error with working directory : %s\n", err)
+		return nil, err
+	}
+	schemaPath, err := ioutil.TempFile(os.TempDir(), "marathon.schema")
+	if err != nil {
+		plugin.SendLog(a, "PLUGIN-MARATHON", "Error marathon schema (%s) : %s\n", schemaPath.Name(), err)
+		return nil, err
+	}
+	defer os.RemoveAll(schemaPath.Name())
+
+	if err := ioutil.WriteFile(schemaPath.Name(), []byte(schema), 0644); err != nil {
+		plugin.SendLog(a, "PLUGIN-MARATHON", "Error marathon schema : %s\n", err)
+		return nil, err
+	}
+
+	var filePath = f
+	if !filepath.IsAbs(f) {
+		filePath = path.Join(wd, f)
+	}
+
+	schemaLoader := gojsonschema.NewReferenceLoader("file://" + schemaPath.Name())
+	documentLoader := gojsonschema.NewReferenceLoader("file://" + filePath)
+
+	result, err := gojsonschema.Validate(schemaLoader, documentLoader)
+	if err != nil {
+		plugin.SendLog(a, "PLUGIN-MARATHON", "Unable to validate document %s\n", err)
+		return nil, err
+	}
+	if !result.Valid() {
+		plugin.SendLog(a, "PLUGIN-MARATHON", "The document is not valid. see following errors\n")
+		for _, desc := range result.Errors() {
+			plugin.SendLog(a, "PLUGIN-MARATHON", " - %s", desc.Details())
+		}
+		return nil, fmt.Errorf("IMarathonPluginnvalid json document")
+	}
+
+	return appConfig, nil
+}
+
+func main() {
+
+	if len(os.Args) == 2 && os.Args[1] == "info" {
+		m := MarathonPlugin{}
+		p := m.Parameters()
+
+		fmt.Printf(" - Name:\t%s\n", m.Name())
+		fmt.Printf(" - Author:\t%s\n", m.Author())
+		fmt.Printf(" - Description:\t%s\n", m.Description())
+		fmt.Println(" - Parameters:")
+		for _, n := range p.Names() {
+			fmt.Printf("\t - %s: %s\n", n, p.GetDescription(n))
+		}
+		return
+	}
+
+	plugin.Serve(&MarathonPlugin{})
+}
