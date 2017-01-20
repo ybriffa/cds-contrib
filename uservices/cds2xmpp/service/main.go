1f539c4295efdb7f22a7b74aebf915732b43b290
Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>
2017-01-20 09:39:11 +0100

    fix: viper args

--- uservices/cds2xmpp/service/main.go
+++ uservices/cds2xmpp/service/main.go
@@ -123,7 +123,7 @@ func init() {
 	viper.BindPFlag("xmpp_insecure_skip_verify", flags.Lookup("xmpp-insecure-skip-verify"))
 
 	flags.String("xmpp-default-hostname", "", "Default Hostname for user, enter your.jabber.net for @your.jabber.net")
-	viper.BindPFlag("xmpp_default_hostname", flags.Lookup("xmpp-default_hostname"))
+	viper.BindPFlag("xmpp_default_hostname", flags.Lookup("xmpp-default-hostname"))
 }
 
 func main() {
